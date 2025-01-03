3acf542948b02b7f173f46aeab1378f9849afe86
srigansystem <96302359+srigansystem@users.noreply.github.com>
2025-01-03 17:13:56 +0000

    yes

--- src/app/HomepageHeading.js
+++ src/app/HomepageHeading.js
@@ -13,7 +13,7 @@ const HomepageHeading = () => {
     <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
       <div className="flex h-16 items-center justify-between">
         <div className="flex-1 md:flex md:items-center md:gap-12">
-          <strong style={{fontSize:"40px"}}>Ewari Times</strong>
+          <img src="./images/logo.jpeg"/>
         </div>
   
         <div className="md:flex md:items-center md:gap-12">
